718a1818023bd97773ebb2222311000e21236227
Jan Mewes <jan.mewes@epages.com>
2018-02-24 17:45:38 +0100

    Apply workaround for editor startup (#4)

--- docker-run-editor.sh
+++ docker-run-editor.sh
@@ -4,13 +4,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Build the project
 mvn clean install
-mvn install
 
 # Start the editor and store the ID of its Docker container
 CONTAINER_ID=$(docker run -d -v ${SCRIPT_DIR}:/home/ksch-workflows -p 5000:8080 cc-editor | tr -d '\n')
 
 # Wait for Tomcat startup
-sleep 2
+sleep 15
 
 # Import the 'ksch-workflows' project into the CaptainCasa editor
 docker cp ${SCRIPT_DIR}/ksch-workflows.xml ${CONTAINER_ID}:/usr/local/tomcat/webapps/editor/config/projects/ksch-workflows.xml

39f39147474ea1ab4f7022fa1832246a3fc05646
Jan Mewes <jan.mewes@ks-plus.org>
2018-03-03 18:53:39 +0100

    Improve documentation

--- src/main/java/io/github/kschworkflows/Dispatcher.java
+++ src/main/java/io/github/kschworkflows/Dispatcher.java
@@ -5,9 +5,11 @@ import lombok.NoArgsConstructor;
 import org.eclnt.workplace.IWorkpageContainer;
 import org.eclnt.workplace.WorkpageDispatcher;
 
-/*
- * The dispatcher is referenced in faces-config.xml. When changing the package
- * of the dispatcher, then also update the faces-config.xml link!
+/**
+ * The purpose of the Dispatcher is to connect the layout files with the page beans.
+ * <p>
+ * It is proclaimed to the CaptainCasa framework via the "faces-config.xml" file.
+ * All packages with page beans need to be added in the "dispatcherinfo.xml" file.
  */
 @NoArgsConstructor
 public class Dispatcher extends WorkpageDispatcher
